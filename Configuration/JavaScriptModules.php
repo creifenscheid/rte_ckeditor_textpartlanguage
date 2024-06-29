57bd3d529b979894b217262a01a5019860a72182
Christian Reifenscheid <dev@christian-reifenscheid.de>
2024-06-29 10:08:11 +0200

    [TASK] Plugin
    Adds plugin files and registration

--- Configuration/JavaScriptModules.php
+++ Configuration/JavaScriptModules.php
@@ -1,8 +1,14 @@
 <?php
 
-$extensionKey = 'dummy_extension';
+$extensionKey = 'cke_textpartlanguage';
 
 return [
+    'dependencies' => [
+        'backend',
+    ],
+    'tags' => [
+        'backend.form',
+    ],
     'imports' => [
         '@creifenscheid/' . str_replace('_', '-', $extensionKey) . '/' => 'EXT:' . $extensionKey . '/Resources/Public/JavaScript/',
     ],
--- ext_localconf.php
+++ ext_localconf.php
@@ -4,4 +4,6 @@ defined('TYPO3') || die();
 
 (static function ($extensionKey) {
     
+    $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['ckeTextpartlanguage'] = 'EXT:' . $extensionKey . '/Configuration/RTE/Configuration.yaml';
+    
 })('cke_textpartlanguage');
